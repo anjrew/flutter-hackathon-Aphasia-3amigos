0c82ed5663a2ca0b4e300d6b040e0a200adef087
lucacorbucci <tankado@live.it>
2020-02-01 15:27:46 +0100

    tts

--- lib/main.dart
+++ lib/main.dart
@@ -22,22 +22,15 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Main Page UI'),
+      home: MyHomePage(
+        title: 'Main Page UI',
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -46,36 +39,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _selectedWord = "";
-  List<String> _array = ["1", "2", "3", "4", "5"];
-  var rnd = new Random();
-  var count = 0;
+  var language = ["🇬🇧", "🇵🇱", "🇩🇪"];
+  var languageCode = ["en-US", "pl", "de"];
+  var currentLanguage = 0;
 
-  @override
-  void initState() {
-    _start();
-  }
-
-  void _start() {
+  void changeLanguage() {
     setState(() {
-      _selectedWord = _array[count];
+      currentLanguage++;
+      if (currentLanguage == language.length) {
+        currentLanguage = 0;
+      }
     });
-  }
-
-  void _nextWord() {
-    setState(() {
-      count++;
-      if (count >= _array.length) count = _array.length - 1;
-      _selectedWord = _array[count];
-    });
-  }
-
-  void _previousWord() {
-    setState(() {
-      count--;
-      if (count < 0) count = 0;
-      _selectedWord = _array[count];
-    });
+    print("Main: " + languageCode[currentLanguage]);
   }
 
   @override
@@ -91,64 +66,24 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
-      ),
-      body: Stack(
-        children: <Widget>[
-          Center(
-            // Center is a layout widget. It takes a single child and positions it
-            // in the middle of the parent.
-            child: Column(
-              // Column is also a layout widget. It takes a list of children and
-              // arranges them vertically. By default, it sizes itself to fit its
-              // children horizontally, and tries to be as tall as its parent.
-              //
-              // Invoke "debug painting" (press "p" in the console, choose the
-              // "Toggle Debug Paint" action from the Flutter Inspector in Android
-              // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-              // to see the wireframe for each widget.
-              //
-              // Column has various properties to control how it sizes itself and
-              // how it positions its children. Here we use mainAxisAlignment to
-              // center the children vertically; the main axis here is the vertical
-              // axis because Columns are vertical (the cross axis would be
-              // horizontal).
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  '$_selectedWord',
-                  style: Theme.of(context).textTheme.headline4,
+        actions: <Widget>[
+          new InkWell(
+            onTap: () {
+              changeLanguage();
+            },
+            child: new Padding(
+              padding: new EdgeInsets.all(8.0),
+              child: new Text(
+                language[currentLanguage],
+                style: new TextStyle(
+                  fontSize: 35.0,
                 ),
-              ],
+              ),
             ),
-          ),
-          BottomButton(
-            alignment: Alignment.bottomLeft,
-            icon: Icons.navigate_before,
-            tooltip: "Back",
-            onButtonPressed: () {
-              _previousWord();
-            },
-          ),
-          BottomButton(
-            alignment: Alignment.bottomCenter,
-            icon: Icons.play_arrow,
-            tooltip: "Hear Sound",
-            onButtonPressed: () {
-              //play();
-            },
-          ),
-          BottomButton(
-            alignment: Alignment.bottomRight,
-            icon: Icons.navigate_next,
-            tooltip: "Next",
-            onButtonPressed: () {
-              _nextWord();
-            },
-          ),
+          )
         ],
       ),
-
-      // This trailing comma makes auto-formatting nicer for build methods.
+      body: WordGame(languageCode[currentLanguage]),
     );
   }
 }
---
+++ lib/pages/word_game.page.dart
@@ -0,0 +1,109 @@
+import 'dart:math';
+import 'package:aphasia_saviour/services/BottomButton.dart';
+import 'package:aphasia_saviour/services/text_to_speech.service.dart';
+import 'package:flutter/material.dart';
+
+class WordGame extends StatefulWidget {
+  var languageCode;
+
+  WordGame(languageCode);
+
+  @override
+  _WordGameState createState() => _WordGameState(languageCode);
+}
+
+class _WordGameState extends State<WordGame> {
+  var languageCode;
+  _WordGameState(this.languageCode);
+
+  String _selectedWord = "";
+  List<String> _array = [
+    "Hallo",
+    "Computer",
+    "Cat",
+    "Dog",
+    "Tiger",
+    "Phone",
+    "House"
+  ];
+  var rnd = new Random();
+  var count = 0;
+  FlutterTts tts = FlutterTts();
+
+  @override
+  void initState() {
+    tts.setLanguage("de-DE");
+    tts.setSpeechRate(0.1);
+    super.initState();
+    _start();
+  }
+
+  void _start() {
+    setState(() {
+      _selectedWord = _array[count];
+    });
+  }
+
+  void _nextWord() {
+    setState(() {
+      count++;
+      if (count >= _array.length) count = _array.length - 1;
+      _selectedWord = _array[count];
+    });
+  }
+
+  void _previousWord() {
+    setState(() {
+      count--;
+      if (count < 0) count = 0;
+      _selectedWord = _array[count];
+    });
+  }
+
+  void _playSound() {
+    tts.speak(_selectedWord);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                '$_selectedWord',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ],
+          ),
+        ),
+        BottomButton(
+          alignment: Alignment.bottomLeft,
+          icon: Icons.navigate_before,
+          tooltip: "Back",
+          onButtonPressed: () {
+            _previousWord();
+          },
+        ),
+        BottomButton(
+          alignment: Alignment.bottomCenter,
+          icon: Icons.play_arrow,
+          tooltip: "Hear Sound",
+          onButtonPressed: () {
+            _playSound();
+          },
+        ),
+        BottomButton(
+          alignment: Alignment.bottomRight,
+          icon: Icons.navigate_next,
+          tooltip: "Next",
+          onButtonPressed: () {
+            _nextWord();
+          },
+        ),
+      ],
+    );
+  }
+}
