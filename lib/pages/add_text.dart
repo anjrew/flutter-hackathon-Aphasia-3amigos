a346c2bb2503cdc1a3e245d087d64dbd43f7dc6c
earyzhe <earyzhe@gmail.com>
2020-02-01 16:02:08 +0100

    Slideable implemented

--- lib/pages/add_text.dart
+++ lib/pages/add_text.dart
@@ -2,6 +2,7 @@ import 'package:aphasia_saviour/resources/keys.dart';
 import 'package:aphasia_saviour/services/shared_preference.service.dart';
 import 'package:aphasia_saviour/services/text_to_speech.service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class AddTextPage extends StatefulWidget {
   @override
@@ -36,10 +37,26 @@ class _AddTextPageState extends State<AddTextPage> {
           color: Colors.black,
         ),
         itemCount: values.length,
-        itemBuilder: (context, index) => ListTile(
-          key: Key(values[index].toString()),
-          title: Text(values[index]),
-          trailing: IconButton(icon: Icon(Icons.surround_sound), onPressed: () => tts.speak(values[index])),
+        itemBuilder: (context, index) => Slidable(
+          key: Key(index.toString()),
+          direction: Axis.horizontal,
+          actionPane: SlidableStrechActionPane(),
+          actionExtentRatio: 0.25,
+          child: ListTile(
+              key: Key(values[index].toString()),
+              title: Text(values[index]),
+              trailing: IconButton(
+                  icon: Icon(Icons.surround_sound),
+                  onPressed: () => tts.speak(values[index])),
+          ),
+          secondaryActions: <Widget>[
+            IconSlideAction(
+              caption: 'Delete',
+              color: Colors.red,
+              icon: Icons.delete,
+              onTap: () => deleteWord(index),
+            ),
+          ],
         ),
       ),
       Positioned(
@@ -68,6 +85,13 @@ class _AddTextPageState extends State<AddTextPage> {
 
   void addTextToList(String word) async {
     this.values.add(word);
+    setState(() {
+      sharedPrefs.setStringList(id: AppKeys.wordsKey, strings: this.values);
+    });
+  }
+
+  void deleteWord(int index) {
+    this.values.removeAt(index);
     setState(() {
       sharedPrefs.setStringList(id: AppKeys.wordsKey, strings: this.values);
     });
